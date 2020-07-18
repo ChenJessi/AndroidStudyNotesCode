da7bbf34e5c1b6e559e962d724eb20206a2d1606
chen188669 <188669@163.com>
2020-07-18 15:52:44 +0800

    Androidvm classloader

--- app/src/main/java/com/chencc/androidstudynotescode/MainActivity.kt
+++ app/src/main/java/com/chencc/androidstudynotescode/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.chencc.androidstudynotescode
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
+import android.util.Log
+import dalvik.system.DexClassLoader
+import dalvik.system.PathClassLoader
 import java.util.zip.ZipFile
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.e("MainActivity", "Activity.class 由： + ${Activity::class.java.classLoader} + 加载" )
+        Log.e("MainActivity", "MainActivity.class 由： + $classLoader + 加载" )
     }
 }
---
+++ app/src/main/java/com/chencc/androidstudynotescode/androidjvm_class/Hotfix.java
@@ -0,0 +1,7 @@
+package com.chencc.androidstudynotescode.androidjvm_class;
+
+/**
+ * android 类加载实例demo
+ */
+public class Hotfix {
+}

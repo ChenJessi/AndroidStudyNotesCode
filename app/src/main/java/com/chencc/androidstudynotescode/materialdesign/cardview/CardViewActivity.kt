5cca44994de513c59b8bff59b941bcd623bf3b05
chen188669 <188669@163.com>
2020-10-28 18:29:25 +0800

    CardView

---
+++ app/src/main/java/com/chencc/androidstudynotescode/materialdesign/cardview/CardViewActivity.kt
@@ -0,0 +1,17 @@
+package com.chencc.androidstudynotescode.materialdesign.cardview
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.chencc.androidstudynotescode.R
+
+
+/**
+ * CardView 练习
+ */
+class CardViewActivity  : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cardview)
+
+    }
+}

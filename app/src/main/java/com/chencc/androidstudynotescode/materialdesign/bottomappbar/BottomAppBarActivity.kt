e5153bc4b66e07736418463bf849b92951da3b18
chen188669 <188669@163.com>
2020-11-01 23:11:54 +0800

    BottomAppBar

--- app/src/main/java/com/chencc/androidstudynotescode/materialdesign/bottomappbar/BottomAppBarActivity.kt
+++ app/src/main/java/com/chencc/androidstudynotescode/materialdesign/bottomappbar/BottomAppBarActivity.kt
@@ -1,8 +1,13 @@
 package com.chencc.androidstudynotescode.materialdesign.bottomappbar
 
+import android.graphics.PorterDuff
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.chencc.androidstudynotescode.R
+import com.chencc.androidstudynotescode.utils.dp2px
+import com.google.android.material.shape.*
+import kotlinx.android.synthetic.main.activity_bottom_appbar.*
 
 /**
  * @author Created by CHEN on 2020/10/28
@@ -15,7 +20,39 @@ class BottomAppBarActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bottom_appbar)
 
+        initBar1()
+        initBar2()
+    }
 
-//        MaterialShapeDrawable
+    private fun initBar1(){
+        val triangleEdgeTreatment = TriangleEdgeTreatment(35f.dp2px, true)
+
+        val shapeModel = ShapeAppearanceModel.Builder()
+            .setTopEdge(triangleEdgeTreatment)
+            .build()
+
+        val drawable = MaterialShapeDrawable(shapeModel)
+        drawable.setTint(ContextCompat.getColor(this, R.color.colorPrimary))
+        bottomAppBar1.background = drawable
+
+        fab1.shapeAppearanceModel = ShapeAppearanceModel()
+        fab1.scaleX = 0.7f
+        fab1.scaleY = 0.7f
+        fab1.rotation  = 45f
+    }
+
+
+    private fun initBar2(){
+
+        val trapezoidEdgeTreatment = TrapezoidEdgeTreatment()
+
+
+        val shapeModel = ShapeAppearanceModel.Builder()
+            .setTopEdge(trapezoidEdgeTreatment)
+            .build()
+        val drawable = MaterialShapeDrawable(shapeModel)
+        drawable.setTint(ContextCompat.getColor(this, R.color.colorPrimary))
+
+        bottomAppBar2.background = drawable
     }
 }
---
+++ app/src/main/java/com/chencc/androidstudynotescode/materialdesign/bottomappbar/TrapezoidEdgeTreatment.kt
@@ -0,0 +1,33 @@
+package com.chencc.androidstudynotescode.materialdesign.bottomappbar
+
+import android.util.Log
+import com.chencc.androidstudynotescode.utils.dp2px
+import com.google.android.material.shape.EdgeTreatment
+import com.google.android.material.shape.ShapePath
+
+/**
+ * @author Created by CHEN on 2020/11/1
+ * @email 188669@163.com
+ * 梯形缺口
+ */
+private const val TAG = "TrapezoidEdgeTreatment"
+class TrapezoidEdgeTreatment : EdgeTreatment() {
+
+    val radius = 10f.dp2px
+    override fun getEdgePath(length: Float, center: Float, interpolation: Float, shapePath: ShapePath) {
+        Log.e(TAG, "getEdgePath:   $interpolation")
+        shapePath.apply {
+
+            addArc(0f, 0f, 80f, 80f, 180f, 90f);
+
+//            lineTo(center - 40f.dp2px, 0f)
+            lineTo(center - 20f.dp2px, 0f)
+            addArc(center - 70f.dp2px,0f, center - 20f.dp2px, 50f.dp2px  , 270f, 45f)
+            lineTo(center - 15f.dp2px, 30f.dp2px)
+            lineTo(center + 15f.dp2px, 30f.dp2px)
+//            lineTo(center + 20f.dp2px, 0f)
+            addArc(center + 15f.dp2px,0f, center + 65f.dp2px, 50f.dp2px  , 225f, 45f)
+            lineTo(length, 0f)
+        }
+    }
+}

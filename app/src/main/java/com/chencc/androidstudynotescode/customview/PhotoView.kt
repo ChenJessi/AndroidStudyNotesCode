815ec848108a006f86b8f0747cf4b811981584df
chen188669 <188669@163.com>
2020-11-14 15:40:08 +0800

    PhotoView

---
+++ app/src/main/java/com/chencc/androidstudynotescode/customview/PhotoView.kt
@@ -0,0 +1,17 @@
+package com.chencc.androidstudynotescode.customview
+
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.view.View
+
+/**
+ * PhotoView
+ */
+class PhotoView(mContext : Context, attrs : AttributeSet? = null)  : View(mContext,attrs) {
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+    }
+}
--- app/src/main/java/com/chencc/androidstudynotescode/utils/Utils.kt
+++ app/src/main/java/com/chencc/androidstudynotescode/utils/Utils.kt
@@ -2,7 +2,10 @@ package com.chencc.androidstudynotescode.utils
 
 import android.content.Context
 import android.content.res.Resources
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.util.TypedValue
+import androidx.annotation.DrawableRes
 import java.io.File
 import java.io.RandomAccessFile
 
@@ -17,3 +20,13 @@ import java.io.RandomAccessFile
     }
 
 
+
+    fun getBitmap(res : Resources, @DrawableRes id : Int, width : Int) : Bitmap{
+        val option = BitmapFactory.Options()
+        option.inJustDecodeBounds = true
+        BitmapFactory.decodeResource(res, id, option)
+        option.inJustDecodeBounds = false
+        option.inDensity = option.outWidth
+        option.inTargetDensity = width
+        return BitmapFactory.decodeResource(res, id, option)
+    }

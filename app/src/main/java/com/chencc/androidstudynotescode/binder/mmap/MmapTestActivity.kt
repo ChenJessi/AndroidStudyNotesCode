a95a4d4785f998ed52046baf7ceb0ab40fe13cd2
chen188669 <188669@163.com>
2020-12-08 00:03:27 +0800

    binder

---
+++ app/src/main/java/com/chencc/androidstudynotescode/binder/mmap/MmapTestActivity.kt
@@ -0,0 +1,29 @@
+package com.chencc.androidstudynotescode.binder.mmap
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.chencc.androidstudynotescode.R
+import com.chencc.androidstudynotescode.databinding.ActivityMmapTestBinding
+
+/**
+ * @author Created by CHEN on 2020/12/7
+ * @email 188669@163.com
+ * mmap 读写文件测试
+ */
+class MmapTestActivity : AppCompatActivity(){
+
+    init {
+        System.loadLibrary("native-lib")
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = DataBindingUtil.setContentView<ActivityMmapTestBinding>(this, R.layout.activity_mmap_test)
+
+    }
+
+    fun write(){}
+    fun read(){}
+    external fun writeTest()
+    external fun readTest()
+}

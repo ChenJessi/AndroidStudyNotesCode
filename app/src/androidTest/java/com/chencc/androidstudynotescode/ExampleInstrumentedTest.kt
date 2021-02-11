47bde18deb0225705968b5d97167bc7f0559a6a9
chen188669 <188669@163.com>
2021-02-11 22:44:35 +0800

    map and array test

--- app/src/androidTest/java/com/chencc/androidstudynotescode/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/chencc/androidstudynotescode/ExampleInstrumentedTest.kt
@@ -1,5 +1,6 @@
 package com.chencc.androidstudynotescode
 
+import android.util.SparseArray
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import org.junit.Assert.assertEquals
@@ -16,7 +17,44 @@ class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
         // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.chencc.androidstudynotescode", appContext.packageName)
+//        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+//        assertEquals("com.chencc.androidstudynotescode", appContext.packageName)
+
+        hashmap()
+        sparseArray()
+    }
+
+    fun hashmap(){
+        val t1 = System.currentTimeMillis()
+        val map = HashMap<Int, String>()
+        for (i in 0..10000){
+            map.put(i, i.toString())
+        }
+        val t2 = System.currentTimeMillis()
+        println("hashmap 放入用时 ： t2 - t1 : ${t2 - t1}")
+
+        for (i in  0..10000){
+            val a = map.get(i)
+        }
+        val t3 = System.currentTimeMillis()
+        println("hashmap 取出用时 ： t3 - t2 : ${t3-t2}")
+
+    }
+
+    fun sparseArray(){
+        val t1 = System.currentTimeMillis()
+        val array = SparseArray<String>()
+        for (i in 0..10000){
+            array.put(i, i.toString())
+        }
+        val t2 = System.currentTimeMillis()
+        println("sparseArray 放入用时 ： t2 - t1 : ${t2 - t1}")
+
+        for (i in 0..10000){
+            val a = array.get(i)
+        }
+        val t3 = System.currentTimeMillis()
+        println("sparseArray 取出用时 ： t3 - t2 : ${t3-t2}")
+
     }
 }
---
+++ app/src/main/java/com/chencc/androidstudynotescode/performance/data_structure/DataStructure.kt
@@ -0,0 +1,49 @@
+package com.chencc.androidstudynotescode.performance.data_structure
+
+import android.util.SparseArray
+
+/**
+ * @author Created by CHEN on 2021/2/11
+ * @email 188669@163.com
+ * 数据结构测试代码
+ */
+
+fun main() {
+    hashmap()
+    // 需要安卓环境
+//    sparseArray()
+}
+
+fun hashmap(){
+    val t1 = System.currentTimeMillis()
+    val map = HashMap<Int, String>()
+    for (i in 0..10000){
+        map.put(i, i.toString())
+    }
+    val t2 = System.currentTimeMillis()
+    println("hashmap 放入用时 ： t2 - t1 : ${t2 - t1}")
+
+    for (i in  0..10000){
+        val a = map.get(i)
+    }
+    val t3 = System.currentTimeMillis()
+    println("hashmap 取出用时 ： t3 - t2 : ${t3-t2}")
+
+}
+
+fun sparseArray(){
+    val t1 = System.currentTimeMillis()
+    val array = SparseArray<String>()
+    for (i in 0..10000){
+        array.put(i, i.toString())
+    }
+    val t2 = System.currentTimeMillis()
+    println("sparseArray 放入用时 ： t2 - t1 : ${t2 - t1}")
+
+    for (i in 0..10000){
+        val a = array.get(i)
+    }
+    val t3 = System.currentTimeMillis()
+    println("sparseArray 取出用时 ： t3 - t2 : ${t3-t2}")
+
+}
